4947371c01be1faf180db57963030082448f566e
igrucci <babichevigor12@gmail.com>
2024-06-18 15:55:59 +0300

    refactor GenerateString method

--- infra/random/service.go
+++ infra/random/service.go
@@ -4,27 +4,22 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/binary"
-	"sync"
 	"x-bank-users/cerrors"
 	"x-bank-users/ercodes"
 )
 
 type Service struct {
-	randomBytes []byte
-	mu          *sync.Mutex
 }
 
 func NewService() Service {
-	return Service{
-		randomBytes: make([]byte, 2),
-		mu:          &sync.Mutex{},
-	}
+	return Service{}
 }
 
 func (s *Service) GenerateString(_ context.Context, set string, size int) (string, error) {
+	randomBytes := make([]byte, 2)
 	res := make([]byte, size)
 	for i := 0; i < size; i++ {
-		randomNum, err := s.GenerateRandomNum()
+		randomNum, err := s.GenerateRandomNum(randomBytes)
 		if err != nil {
 			return "", err
 		}
@@ -35,16 +30,13 @@ func (s *Service) GenerateString(_ context.Context, set string, size int) (strin
 	return string(res), nil
 }
 
-func (s *Service) GenerateRandomNum() (uint16, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	_, err := rand.Read(s.randomBytes)
+func (s *Service) GenerateRandomNum(buf []byte) (uint16, error) {
+	_, err := rand.Read(buf)
 	if err != nil {
 		return 0, cerrors.NewErrorWithUserMessage(ercodes.RandomGeneration, err, "Ошибка генерации случайного числа")
 	}
-
-	num := binary.BigEndian.Uint16(s.randomBytes)
+	
+	num := binary.BigEndian.Uint16(buf)
 
 	return num, nil
 }
