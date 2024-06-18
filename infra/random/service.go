80ebf69e185f69bf296e4137891827157ff83914
igrucci <babichevigor12@gmail.com>
2024-06-18 12:38:35 +0300

    fix GenerateRandomNum method

--- infra/random/service.go
+++ infra/random/service.go
@@ -3,34 +3,48 @@ package random
 import (
 	"context"
 	"crypto/rand"
-	"math/big"
+	"encoding/binary"
+	"sync"
 	"x-bank-users/cerrors"
 	"x-bank-users/ercodes"
 )
 
 type Service struct {
+	randomBytes []byte
+	mu          *sync.Mutex
 }
 
 func NewService() Service {
-	return Service{}
+	return Service{
+		randomBytes: make([]byte, 2),
+		mu:          &sync.Mutex{},
+	}
 }
 
 func (s *Service) GenerateString(_ context.Context, set string, size int) (string, error) {
 	res := make([]byte, size)
 	for i := 0; i < size; i++ {
-		randomNum, err := s.GenerateRandomNum(size)
+		randomNum, err := s.GenerateRandomNum()
 		if err != nil {
-			return "", cerrors.NewErrorWithUserMessage(ercodes.RandomGeneration, err, "Ошибка генерации случайной строки")
+			return "", err
 		}
-		res[i] = set[randomNum]
+
+		res[i] = set[int(randomNum)%len(set)]
 	}
+
 	return string(res), nil
 }
 
-func (s *Service) GenerateRandomNum(n int) (int, error) {
-	num, err := rand.Int(rand.Reader, big.NewInt(int64(n)))
+func (s *Service) GenerateRandomNum() (uint16, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, err := rand.Read(s.randomBytes)
 	if err != nil {
 		return 0, cerrors.NewErrorWithUserMessage(ercodes.RandomGeneration, err, "Ошибка генерации случайного числа")
 	}
-	return int(num.Int64()), nil
+
+	num := binary.BigEndian.Uint16(s.randomBytes)
+
+	return num, nil
 }
